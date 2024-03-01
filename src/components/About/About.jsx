80a409bdf75e4d9c19681389ea355df6f8d97cf5
akiko-luka <ak.luka051@gmail.com>
2024-03-01 17:05:50 +0100

    add components; add css - variables

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <section className="about" id="about">
+      <div className="about-title">
+        <h2>About</h2>
+      </div>
+      <div className="about-content">
+        <p>Hi there! My name is Akiko.</p>
+        <p>I'm currently studying to become a full-stack Web Developer at DCI.</p>
+        <p>I am from Japan and now living in Berlin, love ramen, sushi and am also into anime and taking pictures such as most Japanese, I guess.</p>
+        <p>Being a hard working individual with great attention and focus to details are attributes that I identify myself with, which is probably part of my culture such as animes, taking pictures and smiling.</p>
+        <p>In 2012, I finished my Bachelor's degree in Computer Science but didn't end up in my profession. Now, after moving to Germany, I am giving it a new shot and my goal is to join or work in a fantastic group that will help me to keep growing in this exciting industry.</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import Navbar from "../components/Header/Navbar.jsx"
+import Hero from "../components/Hero/Hero.jsx"
+import About from "../components/About/About.jsx"
+import Skills from "../components/Skills/Skills.jsx"
+import Projects from "../components/Projects/Projects.jsx"
+
+
 const Home = () => {
-  return <div>Home</div>;
+ 
+  return <>
+  <Navbar />
+  <Hero />
+  <About />
+  <Skills />
+  <Projects />
+
+  <footer>&copy; {new Date().getFullYear()} | Akiko Luka</footer>
+  </>;
 };
 
 export default Home;
