1f0210d21974a340aaa094a318eff1b3a683a6a0
akiko-luka <ak.luka051@gmail.com>
2024-03-03 19:48:38 +0100

    add project-section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,27 @@
-import "./Projects.css"
+import "./Projects.css";
+
+// images
+import battleGame from "../../assets/projects/battle-game.jpg"
+import akpica from "../../assets/projects/akpica-screenshot.webp"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <>
+      <section class="project__container scroll" id="projects">
+        <h2 class="project__heading">Projects</h2>
+        <div class="project__content">
+          <div class="project__card">
+            <a href="https://akpica.netlify.app/" target="_blank"><img src={akpica} alt="akpica" />
+            </a>
+          </div>
+            <h3>AKPICA ecommerce</h3>
+          <div class="project__card">
+            <img src={battleGame} alt="battle-game" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
