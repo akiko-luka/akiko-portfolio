76330912ded7e2dcd798bf927e1585bff0f16f89
akiko-luka <ak.luka051@gmail.com>
2024-03-08 20:25:49 +0100

    add down btn

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,16 @@
 import "./Hero.css";
 
+// icon
+import { FaAngleDoubleDown } from "react-icons/fa";
+
 const Hero = () => {
   return (
     <>
-      <section className="hero">
+      <section className="hero__container">
         <h1 className="hero__title">Hi, I'm Akiko!</h1>
+        <div className="hero-btn">
+          <a href="#about"><FaAngleDoubleDown /></a>
+        </div>
       </section>
     </>
   );
