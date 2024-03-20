16b4c8eafcdaad5c2265972e19a0ebac1d557bdf
akiko-luka <ak.luka051@gmail.com>
2024-03-20 21:17:10 +0100

    add animation

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,26 +1,44 @@
+import { useEffect } from "react";
 import "./Projects.css";
 import ProjectCard from "./ProjectCard.jsx";
 import SmallProj from "../SmallProj/SmallProj.jsx";
+
+// AOS - animation
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 // images
 import battleGame from "../../assets/projects/battle-game.jpg";
 import akpica from "../../assets/projects/akpica-screenshot.webp";
 
 const Projects = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1500, easing: "ease-in-out" });
+  }, []);
+
   return (
     <>
       <section className="project__container scroll" id="projects">
         <h2 className="project__header">Projects</h2>
-        <div className="project__content">
+        <div className="project__content" data-aos="fade-up">
+          <ProjectCard
+            image={akpica}
+            altTitle={"akpica-ecommerce"}
+            url={"https://akpica.netlify.app/"}
+            title={"akpica ecommerce"}
+            subTitle={"E-commerce Website"}
+          />
 
-          <ProjectCard image={akpica} altTitle={"akpica-ecommerce"} url={"https://akpica.netlify.app/"} title={"akpica ecommerce"} subTitle={"E-commerce Website"} />
-
-          <ProjectCard image={battleGame} altTitle={"battle-game"} url={"https://github.com/akiko-luka/battle-game"} title={"Dragon⍟Ball 𝐙 Mini Battle Game"} subTitle={"Console-Based App"}/>
-
+          <ProjectCard
+            image={battleGame}
+            altTitle={"battle-game"}
+            url={"https://github.com/akiko-luka/battle-game"}
+            title={"Dragon⍟Ball 𝐙 Mini Battle Game"}
+            subTitle={"Console-Based App"}
+          />
         </div>
-
       </section>
-       <SmallProj />
+      <SmallProj />
     </>
   );
 };
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,42 +1,72 @@
+import { useEffect } from "react";
 import "./Skills.css";
 import SkillsCard from "./SkillsCard.jsx";
 
+// AOS - animation
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 // logos
-import bootstrap from "../../assets/icons/icon-bootstrap.png"
-import css from "../../assets/icons/icon-css.png"
-import express from "../../assets/icons/icon-express.png"
-import git from "../../assets/icons/icon-git.png"
-import github from "../../assets/icons/icon-github.png"
-import html from "../../assets/icons/icon-html.png"
-import mongodb from "../../assets/icons/icon-mongodb.png"
-import js from "../../assets/icons/icon-js.png"
-import markdown from "../../assets/icons/icon-markdown.png"
-import node from "../../assets/icons/icon-node.webp"
-import ps from "../../assets/icons/icon-photoshop.png"
-import react from "../../assets/icons/icon-react.png"
-import sass from "../../assets/icons/icon-sass.png"
-import vscode from "../../assets/icons/icon-vscode.png"
+import bootstrap from "../../assets/icons/icon-bootstrap.png";
+import css from "../../assets/icons/icon-css.png";
+import express from "../../assets/icons/icon-express.png";
+import git from "../../assets/icons/icon-git.png";
+import github from "../../assets/icons/icon-github.png";
+import html from "../../assets/icons/icon-html.png";
+import js from "../../assets/icons/icon-js.png";
+import markdown from "../../assets/icons/icon-markdown.png";
+import mongodb from "../../assets/icons/icon-mongodb.png";
+import node from "../../assets/icons/icon-node.webp";
+import ps from "../../assets/icons/icon-photoshop.png";
+import react from "../../assets/icons/icon-react.png";
+import sass from "../../assets/icons/icon-sass.png";
+import vscode from "../../assets/icons/icon-vscode.png";
 
 const Skills = () => {
+
+  useEffect(() => {
+    AOS.init({ duration: 1500, easing:"ease-in-out" });
+  }, []);
+
   return (
     <>
       <section className="skills__container">
         <h2 className="skills__header">Tech Stack</h2>
-        <div className="skills__content">
-          <SkillsCard logo={html} altTitle={"html"} logoName={"html"}/>
-          <SkillsCard logo={css} altTitle={"css"} logoName={"css"}/>
-          <SkillsCard logo={js} altTitle={"javaScript"} logoName={"javaScript"}/>
-          <SkillsCard logo={node} altTitle={"node.js"} logoName={"node.js"}/>
-          <SkillsCard logo={react} altTitle={"react"} logoName={"react"}/>
-          <SkillsCard logo={git} altTitle={"git"} logoName={"git"}/>
-          <SkillsCard logo={github} altTitle={"github"} logoName={"github"}/>
-          <SkillsCard logo={vscode} altTitle={"vscode"} logoName={"vscode"}/>
-          <SkillsCard logo={markdown} altTitle={"markdown"} logoName={"markdown"}/>
-          <SkillsCard logo={ps} altTitle={"photoshop"} logoName={"photoshop"}/>
-          <SkillsCard logo={bootstrap} altTitle={"bootstrap"} logoName={"bootstrap"}/>
-          <SkillsCard logo={sass} altTitle={"sass"} logoName={"sass"}/>
-          <SkillsCard logo={express} altTitle={"express.js"} logoName={"express.js"}/>
-          <SkillsCard logo={mongodb} altTitle={"mongodb"} logoName={"mongodb"}/>
+        <div className="skills__content" data-aos="fade-up">
+          <SkillsCard
+            logo={js}
+            altTitle={"javaScript"}
+            logoName={"javaScript"}
+          />
+          <SkillsCard
+            logo={mongodb}
+            altTitle={"mongodb"}
+            logoName={"mongodb"}
+          />
+          <SkillsCard
+            logo={express}
+            altTitle={"express.js"}
+            logoName={"express.js"}
+          />
+          <SkillsCard logo={react} altTitle={"react"} logoName={"react"} />
+          <SkillsCard logo={node} altTitle={"node.js"} logoName={"node.js"} />
+          <SkillsCard logo={html} altTitle={"html"} logoName={"html"} />
+          <SkillsCard logo={css} altTitle={"css"} logoName={"css"} />
+          <SkillsCard logo={git} altTitle={"git"} logoName={"git"} />
+          <SkillsCard logo={github} altTitle={"github"} logoName={"github"} />
+          <SkillsCard logo={vscode} altTitle={"vscode"} logoName={"vscode"} />
+          <SkillsCard
+            logo={markdown}
+            altTitle={"markdown"}
+            logoName={"markdown"}
+          />
+          <SkillsCard logo={ps} altTitle={"photoshop"} logoName={"photoshop"} />
+          <SkillsCard
+            logo={bootstrap}
+            altTitle={"bootstrap"}
+            logoName={"bootstrap"}
+          />
+          <SkillsCard logo={sass} altTitle={"sass"} logoName={"sass"} />
         </div>
       </section>
     </>
